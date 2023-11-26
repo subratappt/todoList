004ed0a824fb0beee58e49f2f2758ed9a524942d
Subrata Das <subratappt@gmail.com>
2023-11-25 22:22:13 -0500

    unclean final commit

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Get the input box, list containers, and initialize the listMain array
 const inputBox = document.getElementById("input-box");
 const listData = document.getElementById("list-container");
 const todayData = document.getElementById("today-container");
@@ -5,11 +6,12 @@ const pendingData = document.getElementById("pending-container");
 const completedData = document.getElementById("completed-container");
 let listMain = [];
 
-// Function to add an element from input box to the list container
+// Function to add an element from the input box to the list
 function addElementFromInput() {
     const inputText = inputBox.value.trim();
 
     if (inputText !== "") {
+        // Create a new element object with the input text and default tags
         const element = {
             text: inputText,
             tags: {
@@ -18,24 +20,19 @@ function addElementFromInput() {
                 completed: false
             }
         };
+        // Add the element to the listMain array
         listMain.push(element);
+        // Create a list item element for the element and append it to the list containers
         const listItem = createListItem(element, listMain.length - 1);
-        // Append the list item to the correct container based on its tags
-        if (element.tags.today) {
-            todayData.appendChild(listItem);
-        } else if (element.tags.pending) {
-            pendingData.appendChild(listItem);
-        } else if (element.tags.completed) {
-            completedData.appendChild(listItem);
-        }
-        // updateTags(listMain, listData);
-
-        // listItem.tags.today = false; 
+        appendListItemToContainer(listItem, element.tags);
         listData.appendChild(listItem.cloneNode(true));
-        inputBox.value = ""; // Clear the input box after adding the text
+        // Clear the input box and update the list items in the containers
+        inputBox.value = "";
         updateListItemInContainers(listItem);
+        // Save the list data to local storage
         saveListData();
     } else {
+        // Show an error message if the input is empty
         Swal.fire({
             title: "Error",
             text: "Please enter something to add to the list",
@@ -45,154 +42,107 @@ function addElementFromInput() {
     }
 }
 
-// Function to create the tag elements for a list item
+// Function to create tag elements for a list item
 function createTagElements(listItem, index) {
-    // Parse the tags from the data-tags attribute
     const tags = JSON.parse(listItem.dataset.tags);
 
-
-    // Create a span for each tag that is false
     for (let tag in tags) {
         if (!tags[tag]) {
+            // Create a tag element for each tag that is not set to true
             const tagElement = document.createElement("span");
             tagElement.textContent = "move to " + tag;
             tagElement.classList.add("tag");
             tagElement.classList.add(`tag-${tag}`);
-
-            // Attach the event listener to the tag element
+            updateTags(listMain, listData);
+            // Add a click event listener to update the tags and list item data
             tagElement.addEventListener("click", function () {
-                // Set all tags to false
                 for (let tagKey in tags) {
                     tags[tagKey] = false;
                 }
-
-                // Set the clicked tag to true
                 tags[tag] = true;
-
-                // Update the data-tags and title attributes
                 listItem.dataset.tags = JSON.stringify(tags);
-                listItem.title = `Tags: ${JSON.stringify(tags)}`;
-
-                // Update the list item in the containers
-                updateListItemInContainers(listItem);
+                // listItem.title = `Tags: ${JSON.stringify(tags)}`;
                 listMain[index].tags = tags;
-
-                // Update the tags in the listData
-                updateTags(listMain, listData);
+                updateListItemInContainers(listItem);
                 saveListData();
-
             });
-
             listItem.appendChild(tagElement);
         }
     }
 }
 
-// Function to create a list item with tags
+// Function to create a list item element for an element object
 function createListItem(element, index) {
     const listItem = document.createElement("li");
     listItem.textContent = element.text;
     listItem.dataset.tags = JSON.stringify(element.tags);
-    listItem.dataset.index = index; // Store the index in a data attribute
-    listItem.title = `Tags: ${JSON.stringify(element.tags)}`;
-
-    // Create the tag elements for the list item
+    listItem.dataset.index = index;
+    // listItem.title = `Tags: ${JSON.stringify(element.tags)}`;
     createTagElements(listItem, index);
-
-
-
     return listItem;
 }
 
-
-// Function to update a list item in the containers
+// Function to update the list item in the containers
 function updateListItemInContainers(listItem) {
-    // Remove the list item from all containers
     listItem.remove();
-
-    // Remove the existing tag elements
     const tagElements = listItem.getElementsByClassName("tag");
     while (tagElements.length > 0) {
         tagElements[0].remove();
     }
-
-    // Create the tag elements for the list item
     const index = parseInt(listItem.dataset.index, 10);
     createTagElements(listItem, index);
-
-    // Append the list item to the correct container based on its tags
     const tags = JSON.parse(listItem.dataset.tags);
-    if (tags.today) {
-        todayData.appendChild(listItem);
-    } else if (tags.pending) {
-        pendingData.appendChild(listItem);
-    } else if (tags.completed) {
-        completedData.appendChild(listItem);
-    }
-
-
+    appendListItemToContainer(listItem, tags);
 }
 
+// Function to update the tags in the list containers
 function updateTags(listMain, listContainer) {
-    // Clear the listContainer of any existing tags
     listContainer.innerHTML = '';
 
-    // Loop over each item in listMain
     for (let i = 0; i < listMain.length; i++) {
         const item = listMain[i];
-        // Parse the tags from the tags property
         const listItem = document.createElement("li");
         listItem.textContent = item.text;
         listItem.dataset.tags = JSON.stringify(item.tags);
-        listItem.dataset.index = i; // Store the index in a data attribute
+        listItem.dataset.index = i;
         const tags = item.tags;
 
-        // For each tag, create a span element if the tag value is false
         for (let tag in tags) {
             if (tags[tag]) {
                 const tagElement = document.createElement("span");
                 tagElement.textContent = tag;
                 tagElement.classList.add("tag");
                 tagElement.classList.add(`tag-${tag}`);
-
-                // Append the span to the listContainer
                 listItem.appendChild(tagElement);
             }
         }
         const crossButton = document.createElement("cross-button");
-        crossButton.innerHTML = "\u00d7"; // Unicode value for a cross
-        crossButton.classList.add("cross-button"); // Add a class for styling
-        listItem.appendChild(crossButton); // Append the button to the li
+        crossButton.innerHTML = "\u00d7";
+        crossButton.classList.add("cross-button");
+        listItem.appendChild(crossButton);
         listContainer.appendChild(listItem);
     }
 }
 
+// Function to update the containers with the list data
 function updateContainers() {
-    // Clear all containers
     todayData.innerHTML = '';
     pendingData.innerHTML = '';
     completedData.innerHTML = '';
 
-    // Repopulate the containers based on the current state of listMain
     listMain.forEach((item, index) => {
         const { text, tags = {} } = item;
         const listItem = createListItem({ text, tags }, index);
-
-        if (tags.today) {
-            todayData.appendChild(listItem);
-        } else if (tags.pending) {
-            pendingData.appendChild(listItem);
-        } else if (tags.completed) {
-            completedData.appendChild(listItem);
-        }
+        appendListItemToContainer(listItem, tags);
     });
 
-    // Update the tags in listData
     updateTags(listMain, listData);
 }
 
+// Event listener for handling element click events
 function handleElementClick(event) {
     if (event.target.tagName === "CROSS-BUTTON") {
+        // Show a confirmation dialog before deleting the item
         Swal.fire({
             title: "Confirmation",
             text: "Are you sure you want to delete this item?",
@@ -203,38 +153,35 @@ function handleElementClick(event) {
             cancelButtonText: "No",
         }).then((result) => {
             if (result.isConfirmed) {
-                // Get the index of the list item element
+                // Get the index of the item and remove it from the listMain array
                 const index = Array.from(listData.children).indexOf(event.target.parentElement);
-
-                // Remove the corresponding item from listMain
                 listMain.splice(index, 1);
-
-                // Update all containers
-                // updateContainers();
-
-                // Save the updated listMain to local storage
+                // Save the list data and reload the list
                 saveListData();
                 loadListData();
             }
         });
-    } else if (event.target.tagName === "LI") {
-        event.target.classList.toggle("checked");
-        saveListData();
     }
+    //  else if (event.target.tagName === "LI") {
+    //     // Toggle the "checked" class of the list item and save the list data
+    //     event.target.classList.toggle("checked");
+    //     saveListData();
+    // }
 }
-// Add event listener
+
+// Add the click event listener to the list container
 listData.addEventListener("click", handleElementClick);
 
-// Function to save listMain to local storage
+// Function to save the list data to local storage
 function saveListData() {
     localStorage.setItem('todoList', JSON.stringify(listMain));
 }
 
+// Function to load the list data from local storage
 function loadListData() {
     const data = JSON.parse(localStorage.getItem("todoList"));
 
     if (data) {
-        // Clear listMain and listData
         listMain = [];
         listData.innerHTML = '';
         todayData.innerHTML = '';
@@ -242,35 +189,20 @@ function loadListData() {
         completedData.innerHTML = '';
 
         data.forEach((dataItem) => {
-            console.log(dataItem);
-            // Add the data item to listMain
             listMain.push(dataItem);
-
-            // Create a list item and append it to the correct container based on its tags
-            const { text, tags = {} } = dataItem; // Set tags to an empty object if it's undefined
+            const { text, tags = {} } = dataItem;
             const listItem = createListItem({ text, tags }, listMain.length - 1);
-
-            if (dataItem.tags.today) {
-                todayData.appendChild(listItem);
-            } else if (dataItem.tags.pending) {
-                pendingData.appendChild(listItem);
-            } else if (dataItem.tags.completed) {
-                completedData.appendChild(listItem);
-            }
+            appendListItemToContainer(listItem, tags);
         });
 
-        // Update the tags in the listData
         updateTags(listMain, listData);
     }
 }
 
-
-
-
-// Call loadListData when the page loads
+// Load the list data when the DOM is loaded
 document.addEventListener('DOMContentLoaded', loadListData);
 
-// Function to download the todoList
+// Function to download the todo list as a JSON file
 function downloadTodoList() {
     const data = localStorage.getItem("todoList");
     const blob = new Blob([data], { type: 'application/json' });
@@ -284,7 +216,7 @@ function downloadTodoList() {
     URL.revokeObjectURL(url);
 }
 
-// Function to upload the todoList
+// Function to upload a todo list from a JSON file
 function uploadTodoList(event) {
     const file = event.target.files[0];
     const reader = new FileReader();
@@ -293,13 +225,25 @@ function uploadTodoList(event) {
         const data = event.target.result;
         const list = JSON.parse(data);
         localStorage.setItem("todoList", JSON.stringify(list));
-        // You'll also need to update your list variable and re-render the list
         loadListData();
     };
 
     reader.readAsText(file);
 }
 
-// Add event listeners to your download and upload buttons
+// Add event listeners for the download and upload buttons
 document.querySelector('#downloadButton').addEventListener('click', downloadTodoList);
 document.querySelector('#uploadInput').addEventListener('change', uploadTodoList);
+
+// Function to append a list item to the appropriate container based on its tags
+function appendListItemToContainer(listItem, tags) {
+    if (tags.today) {
+        todayData.appendChild(listItem);
+    } else if (tags.pending) {
+        pendingData.appendChild(listItem);
+    } else if (tags.completed) {
+        completedData.appendChild(listItem);
+    }
+}
+
+document.getElementById('date').textContent = new Date().toLocaleDateString('en-US', { year: 'numeric', month: 'short', day: 'numeric' });
